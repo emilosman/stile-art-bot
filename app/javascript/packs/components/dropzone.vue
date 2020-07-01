f13ca35ed0a799135e8a3a399cfc2cabb34bbc5a
Emil Osmanbegovic <emilosmanbegovic@gmail.com>
2020-07-01 19:08:48 +0200

    add board id to meta

--- app/javascript/packs/components/dropzone.vue
+++ app/javascript/packs/components/dropzone.vue
@@ -17,7 +17,7 @@
             thumbnailWidth: 150,
             paramName: "image",
             params: {
-              "board_id": '079364c4-bb4b-4d5b-bc8d-e6d1a643bbba',
+              "board_id": document.querySelector('meta[name="board-id"]').getAttribute('content')
             },
             headers: {
               "X-CSRF-Token": document.querySelector('meta[name="csrf-token"]').getAttribute('content')
