1407eb00a9dac2736d9dc49b3074f884a110b382
Emil Osmanbegovic <emilosmanbegovic@gmail.com>
2020-07-01 15:21:39 +0200

    add pages

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,4 +1,6 @@
 class BoardsController < ApplicationController
+  before_action :find_board, only: [:show, :edit]
+
   def index
     @boards = Board.all
   end
@@ -8,12 +10,21 @@ class BoardsController < ApplicationController
   end
 
   def create
-    board = Board.create(title: params[:title])
+    board = Board.create(title: board_params[:title])
 
     redirect_to board
   end
 
-  def show
+  def show;end
+
+  def edit;end
+
+  private
+  def find_board
     @board = Board.find params[:id]
+  end
+
+  def board_params
+    params.require(:board).permit(:title)
   end
 end
