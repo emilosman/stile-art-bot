82561af4d7e83ef86f14fdf810408a2d9aaf8206
Emil Osmanbegovic <emilosmanbegovic@gmail.com>
2020-07-09 16:00:13 +0200

    ability to add images from chrome extension

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,27 @@
 class ItemsController < ApplicationController
+  require 'open-uri'
+
   before_action :find_item, only:[:update, :destroy]
+  skip_before_action :verify_authenticity_token, only: [:create]
 
   def create
-    item = Item.new permit_params
+    if params[:image_url]
+      image = open(params[:image_url])
+    else params[:image]
+      image = params[:image]
+    end
+
+    item = Item.new(
+      board_id: params[:board_id],
+      position: params[:position],
+      text: params[:text]
+    )
+
+    item.image.attach(io: image, filename:'upload')
+
     last_position = Board.find(params[:board_id]).items.maximum('position') || 0
     item.position = last_position + 1
-    item.save
+    item.save!
 
     render json: item.to_json({methods: :image_url}), status: 200
   end
