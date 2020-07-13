cfbca8d64abb86f681cc5a773eacfc76f19805d1
Emil Osmanbegovic <emilosmanbegovic@gmail.com>
2020-07-13 13:17:18 +0200

    use urk_for

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,6 +8,6 @@ class Item < ApplicationRecord
   default_scope { order(position: :asc) }
 
   def image_url
-    rails_blob_path(image, disposition: 'attachment') if image.present?
+    url_for(image) if image.present?
   end
 end
