c42318ccbf14224f712672ca5d2e4d08a6766ed5
Emil Osmanbegović <hello@emilosman.com>
2022-02-18 13:57:19 +0100

    auth user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+
+  def authenticate_user!
+    redirect_to new_user_session_path, alert: "You need to login to access that page." unless user_signed_in?
+  end
 end
