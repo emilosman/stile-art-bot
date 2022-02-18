c50b6f0764aed03a89650b22eedea6d5d61e27d8
Emil Osmanbegović <hello@emilosman.com>
2022-02-18 12:38:50 +0100

    add bot to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ApplicationRecord
-  has_many :boards
-  has_many :twitter_bots
+  has_one :board
+  has_one :twitter_bot
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: %i[twitter] # add these two! 
+
+  after_save :check_bot
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
@@ -12,6 +14,9 @@ class User < ApplicationRecord
       user.password = Devise.friendly_token[0, 20]
       user.name = auth.info.name
       user.username = auth.info.nickname
+      user.access_token = auth.credentials.token
+      user.access_token_secret = auth.credentials.secret
+
 
       # user.image = auth.info.image # assuming the user model has an image
       # If you are using confirmable and the provider(s) you use validate emails,
@@ -22,5 +27,15 @@ class User < ApplicationRecord
 
   def email_required?
     false
+  end
+
+  private
+  def check_bot
+    TwitterBot.find_or_create_by(
+      user_id: id,
+      name: name,
+      access_token: access_token,
+      access_token_secret: access_token_secret
+    )
   end
 end
---
+++ db/migrate/20220218112921_add_tokens_to_user.rb
@@ -0,0 +1,6 @@
+class AddTokensToUser < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :access_token, :string
+    add_column :users, :access_token_secret, :string
+  end
+end
