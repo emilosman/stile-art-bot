9a33a55fc23cc58c15ec68239e66708ea01dd1a3
Emil Osmanbegović <hello@emilosman.com>
2022-02-18 22:28:44 +0100

    save username for board

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,6 @@ class User < ApplicationRecord
       user.access_token = auth.credentials.token
       user.access_token_secret = auth.credentials.secret
 
-
       # user.image = auth.info.image # assuming the user model has an image
       # If you are using confirmable and the provider(s) you use validate emails,
       # uncomment the line below to skip the confirmation emails.
@@ -34,14 +33,14 @@ class User < ApplicationRecord
   def check_bot
     TwitterBot.find_or_create_by(
       user_id: id,
-      name: name,
+      name: username,
       access_token: access_token,
       access_token_secret: access_token_secret
     )
 
     Board.find_or_create_by(
       user_id: id,
-      title: name
+      title: username
     )
   end
 end
