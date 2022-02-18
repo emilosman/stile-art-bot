1e99afd9d46c119bc778c5096a91964060b0f0c3
Emil Osmanbegović <hello@emilosman.com>
2022-02-18 16:34:45 +0100

    add host

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   default_url_options only_path: false
-  default_url_options host: "localhost:3000"
+  default_url_options host: "localhost:3000" if Rails.env.development?
+  default_url_options host: "https://stile-art-bot.herokuapp.com" if Rails.env.production?
   
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', sessions: 'users/sessions' }
 
